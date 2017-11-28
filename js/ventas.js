8592c1359df7a6318042c78446006e8c53ac8926
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2017-11-28 11:34:01 -0600

    Se agregan parámetros

--- js/ventas.js
+++ js/ventas.js
@@ -505,6 +505,18 @@ function panelVentas(){
 			},function(error){
 				alertify.error("Ocurrió un error al leer el código");
 				console.log(error);
+			}, {
+				preferFrontCamera : true, // iOS and Android
+				showFlipCameraButton : true, // iOS and Android
+				showTorchButton : true, // iOS and Android
+				torchOn: true, // Android, launch with the torch switched on (if available)
+				saveHistory: true, // Android, save scan history (default false)
+				prompt : "Coloque un código de barras dentro del área de escaneo", // Android
+				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+				formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+				orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+				disableAnimations : true, // iOS
+				disableSuccessBeep: false // iOS and Android
 			});
 		});
 	});
