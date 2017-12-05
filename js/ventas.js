dd9c824c3b594efe29c5ee43c5fc903826f9152f
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2017-12-05 13:48:10 -0600

    Actualizando parámetros del sistema

--- js/ventas.js
+++ js/ventas.js
@@ -516,14 +516,14 @@ function panelVentas(){
 				alertify.error("Ocurrió un error al leer el código");
 				console.log(error);
 			}, {
-				preferFrontCamera : true, // iOS and Android
+				preferFrontCamera : false, // iOS and Android
 				showFlipCameraButton : true, // iOS and Android
 				showTorchButton : true, // iOS and Android
 				torchOn: true, // Android, launch with the torch switched on (if available)
 				saveHistory: true, // Android, save scan history (default false)
 				prompt : "Coloque un código de barras dentro del área de escaneo", // Android
 				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-				formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+				formats : "QR_CODE,DATA_MATRIX,UPC_E,UPC_A,EAN_8,EAN_13,CODE_128,CODE_39,CODE_93,CODABAR,ITF", // default: all but PDF_417 and RSS_EXPANDED
 				orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
 				disableAnimations : true, // iOS
 				disableSuccessBeep: false // iOS and Android
